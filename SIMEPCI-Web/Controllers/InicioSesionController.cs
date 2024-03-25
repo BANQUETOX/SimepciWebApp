81653b15d7039e03e053166d175129640b9f6ca3
gacunal <gacunal@ucenfotec.ac.cr>
2024-03-24 21:49:30 -0600

    Creación de archivos cshtml,css y js relacionadas a la vista Iniciar Sesion

---
+++ SIMEPCI-Web/Controllers/InicioSesionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SIMEPCI_Web.Controllers
+{
+    public class InicioSesionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult InicioSesion()
+        {
+            return View();
+        }
+    }
+}
---
+++ SIMEPCI-Web/wwwroot/js/Pages/InicioSesion/InicioSesion.js
@@ -0,0 +1,31 @@
+﻿function InicioSesion() {
+    this.InitView = function () {
+
+        $('#btnIniciarSesion').click(function () {
+
+            var view = new InicioSesion();
+            view.IniciarSesion();
+        });
+    }
+
+    this.IniciarSesion = function () {
+        var correo = $('#txtEmail').val().trim();
+        var password = $('#txtPassword').val().trim();
+        if (correo == "" || password == "") {
+            Swal.fire({
+                icon: 'error',
+                title: 'Ha ocurrido un error',
+                text: 'Llenar la informacion para el inicio de sesión'
+            })
+        }
+        this.limpiarCampos();
+    };
+    this.limpiarCampos = function () {
+        $('#txtPassword').val("");
+        $('#txtEmail').val("");
+    };
+}
+$(document).ready(function () {
+    var view = new InicioSesion();
+    view.InitView();
+});
