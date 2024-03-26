78012afe55fa1920189cd3d8b40719745e15fb85
FelipeMena <fmenas@ucenfotec.ac.cr>
2024-03-26 12:44:19 -0600

    Creacion de dashboard general

---
+++ SIMEPCI-Web/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SIMEPCI_Web.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DashBoard()
+        {
+            return View();
+        }
+    }
+}
