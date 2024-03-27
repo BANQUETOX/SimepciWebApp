0b2e0ce4908f7c8bd755b571f8b8eaaf4e78579e
gacunal <gacunal@ucenfotec.ac.cr>
2024-03-27 13:51:48 -0600

    Recetas medicas creacion

---
+++ SIMEPCI-Web/Controllers/RecetasController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SIMEPCI_Web.Controllers
+{
+    public class RecetasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult RegistrarReceta()
+        {
+            return View();
+        }
+    }
+}
