8f89b3089e779917c6ac0fb10569e71b2e93090f
gacunal <gacunal@ucenfotec.ac.cr>
2024-04-04 22:19:19 -0600

    pruebaValidarOTP

--- SIMEPCI-Web/wwwroot/js/Pages/RecuperarContrasenna/ValidarOTP.js
+++ SIMEPCI-Web/wwwroot/js/Pages/RecuperarContrasenna/ValidarOTP.js
@@ -1,31 +1,47 @@
-﻿$(document).ready(function () {
-    $('#formValidarOTP').submit(function (event) {
-        event.preventDefault();
-
-        var otp = $('#otp').val().trim();
-
-        $.ajax({
-            url: 'https://simepciapii.azurewebsites.net/api/RecuperarPasswordOtp/ValidarPasswordOtp',
-            method: 'POST',
-            headers: {
-                'accept': 'text/plain'
-            },
-            data: JSON.stringify({ otp: otp }),
-            contentType: 'application/json;charset=utf-8',
-            dataType: 'json'
-        }).done(function (result) {
-            window.location.href = '/RecuperarContrasenna/CrearNuevaContrasenna';
-        }).fail(function (jqXHR, textStatus, errorThrown) {
-            console.error('Error:', textStatus, errorThrown);
-            console.error('Response:', jqXHR.responseJSON);
-
-            var errorMessage = 'El OTP ingresado no es válido. Por favor, intente nuevamente.';
-
-            if (jqXHR.responseJSON && jqXHR.responseJSON.message) {
-                errorMessage = jqXHR.responseJSON.message;
-            }
-
-            alert(errorMessage);
+﻿function ValidarOTP() {
+    this.InitView = function () {
+        $('#btnValidarOtp').click(function () {
+            var view = new ValidarOTP();
+            view.ValidarOtp();
         });
-    });
+    }
+
+    this.ValidarOtp = function () {
+        var mail = $('#email').val();
+        var otp = $('#otp').val();
+        var data = {
+            correo: mail,
+            otpInput:otp
+        };
+        $('#espera').show();
+        url_base = 'https://simepciapii.azurewebsites.net/api/RecuperarPasswordOtp/ValidarPasswordOtp';
+        $.ajax({
+            headers: {
+                'Accept': "text/plain",
+                'Content-Type': "application/json"
+            },
+            method: 'GET',
+            url: url_base+'?correo='+correo+'&otpInput='+otp,
+            contentType: 'application/json;charset=utf-8',
+            dataType: 'json',
+            data: JSON.stringify(data),
+            hasContent: true
+        }).done(function (result) {
+            if (result === "true") {
+                window.location = '/RecuperarContrasenna/CrearNuevaContrasenna';
+            } else {
+                alert('El OTP no es válido');
+            }
+        }).fail(function (error) {
+            console.log(error);
+            alert('Ocurrió un error al validar el OTP');
+        }).always(function () {
+            $('#espera').hide();
+        });
+    }
+}
+
+$(document).ready(function () {
+    var view = new ValidarOTP();
+    view.InitView();
 });
