5abf220bff47785dcc2a42d75e8dd11870dc03a0
gacunal <gacunal@ucenfotec.ac.cr>
2024-04-05 20:01:45 -0600

    Inicio Creacion vista de configuraciones

---
+++ SIMEPCI-Web/Controllers/ConfiguracionesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SIMEPCI_Web.Controllers
+{
+    public class ConfiguracionesController : Controller
+    {
+        public IActionResult Configuraciones()
+        {
+            return View();
+        }
+    }
+}
