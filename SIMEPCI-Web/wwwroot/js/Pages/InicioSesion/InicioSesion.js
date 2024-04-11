255eb07d27104d4f1b5a6ae85aaffe6aa1a6af64
gacunal <gacunal@ucenfotec.ac.cr>
2024-04-10 18:54:21 -0600

    Cambios de inicioSesion

--- SIMEPCI-Web/wwwroot/js/Pages/InicioSesion/InicioSesion.js
+++ SIMEPCI-Web/wwwroot/js/Pages/InicioSesion/InicioSesion.js
@@ -1,24 +1,24 @@
 ﻿function InicioSesion() {
+    var correo = $('#txtEmail').val().trim();
+    var password = $('#txtPassword').val().trim();
     this.InitView = function () {
-
         $('#btnIniciarSesion').click(function () {
             var view = new InicioSesion();
             view.IniciarSesion();
+            view.GetUserByEmail();
         });
     }
-
     this.IniciarSesion = function () {
-        var correo = $('#txtEmail').val().trim();
-        var password = $('#txtPassword').val().trim();
         var url_base = 'https://simepciapii.azurewebsites.net/api/Usuario/Login';
         console.log(correo);
         console.log(password);
         console.log(url_base);
+        console.log(sessionStorage.getItem('usuario'));
         $.ajax({
             url: url_base + '?correo=' + correo + '&password=' + password,
             method: 'GET',
             contentType: 'application/json;charset=utf-8',
-            dataType:"json"
+            dataType: "json"
         }).done(function (result) {
             if (result.activo === true) {
                 window.location = "/Perfil/Perfil";
@@ -39,6 +39,22 @@
         });
         this.limpiarCampos();
     };
+    this.GetUserByEmail = function () {
+        var url_base = 'https://simepciapii.azurewebsites.net/api/Usuario/GetUsuarioByCorreo?correo='
+        $.ajax({
+            url: url_base + correo,
+            method: 'GET',
+            contentType: 'application/json;charset=utf-8',
+            dataType: "json"
+        }).done(function (result) {
+            console.log(result);
+            if (result) {
+                sessionStorage.setItem('usuario', JSON.stringify(result))
+            }
+        }).fail(function (error) {
+            console.log(error);
+        });
+    }
     this.limpiarCampos = function () {
         $('#txtPassword').val("");
         $('#txtEmail').val("");
