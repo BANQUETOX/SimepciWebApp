0a443d32444a5270343de6c7bdcd15a03a98661c
quened29 <kriverac@ucenfotec.ac.cr>
2024-04-21 12:46:16 -0600

    updates respecto al editar sede

--- SIMEPCI-Web/wwwroot/js/Pages/Gestion/GestionSedes.js
+++ SIMEPCI-Web/wwwroot/js/Pages/Gestion/GestionSedes.js
@@ -35,10 +35,7 @@ function initMap() {
 
 // Cloudinary
 $(document).ready(function () {
-    $('#sedesForm').submit(function (event) {
-        event.preventDefault();
-    });
-
+   
     var cloudinaryWidget = cloudinary.createUploadWidget({
         cloudName: 'dddka6gqc',
         uploadPreset: 'ml_default',
@@ -62,6 +59,48 @@ $(document).ready(function () {
         cloudinaryWidget.open();
     });
 
+    var sedeId = null;
+
+    $('.container .row').on('click', '.btn-editar', function () {
+        var sedeCard = $(this).closest('.card-body');
+        var sede = {
+            nombre: sedeCard.find('.nombre').text(),
+            descripcion: sedeCard.find('.descripcion').text(),
+            fechaCreacion: sedeCard.find('.fechaCreacion').text(),
+            provincia: sedeCard.find('.provincia').text(),
+            canton: sedeCard.find('.canton').text(),
+            distrito: sedeCard.find('.distrito').text(),
+            ubicacion: sedeCard.find('.ubicacion').text(),
+        };
+
+        //var sedeId = $(this).data('id');
+
+        $('#nombre').val(sede.nombre);
+        $('#descripcion').val(sede.descripcion);
+        $('#fechaCreacion').val(sede.fechaCreacion);
+        $('#provincia').val(sede.provincia);
+        $('#canton').val(sede.canton);
+        $('#distrito').val(sede.distrito);
+        $('#ubicacion').val(sede.ubicacion);
+
+        $('#registroModal').modal('show');
+    });
+    
+    $('#sedesForm').submit(function (event) {
+        event.preventDefault(); 
+
+       // var sedeId = $(this).data('id');
+
+        if (sedeId) {
+            editarSede(sedeId); 
+        } else {
+            obtenerDatos(); 
+        }
+
+        //obtenerDatos(); 
+    });
+
+    
     //registrar sede
     function obtenerDatos() {
         var sede = {
@@ -74,6 +113,7 @@ $(document).ready(function () {
             ubicacion: $('#ubicacion').val(),
             foto: $('#registroSedes').val(),
         };
+
 
         $.ajax({
             type: 'POST',
@@ -88,6 +128,10 @@ $(document).ready(function () {
                 });
 
                 console.log('Registro exitoso:', response);
+
+
+                // Agregar la nueva tarjeta de sede al DOM
+                agregarTarjetaSede(sede);
             },
             error: function (xhr, status, error) {
                 if (error) {
@@ -99,12 +143,72 @@ $(document).ready(function () {
         });
     }
 
-    $('#sedesForm').submit(function (event) {
-        event.preventDefault();
-        obtenerDatos();
-    });
+    // Función para editar una sede
+    function editarSede(sedeId) {
+        var sede = {
+            id: sedeId,
+            nombre: $('#nombre').val(),
+            descripcion: $('#descripcion').val(),
+            fechaCreacion: $('#fechaCreacion').val(),
+            provincia: $('#provincia').val(),
+            canton: $('#canton').val(),
+            distrito: $('#distrito').val(),
+            ubicacion: $('#ubicacion').val(),
+            foto: $('#registroSedes').val(),
+        };
+
+        $.ajax({
+            type: 'PUT',
+            url: 'https://simepciapii.azurewebsites.net/api/Sede/ActualizarSede',
+            data: JSON.stringify(sede),
+            contentType: 'application/json',
+            success: function (response) {
+                Swal.fire({
+                    icon: 'success',
+                    title: '¡Edición exitosa!',
+                    text: 'La sede se ha actualizado correctamente.'
+                });
+
+                console.log('Edición exitosa:', response);
+
+            },
+            error: function (xhr, status, error) {
+                if (error) {
+                    console.error('Error al editar la sede:', error);
+                } else {
+                    console.error('Error al editar la sede: Error no definido');
+                }
+            }
+        });
+    }
+
+    function agregarTarjetaSede(sede) {
+        var nuevaSedeHTML = `
+    <div class="col-md-6">
+        <div class="card mb-3">
+            <div class="card-body">
+                <h5 class="card-title">Información de la Sede Registrada</h5>
+                <p class="card-text">
+                    <strong>Nombre:</strong> ${sede.nombre}<br>
+                    <strong>Descripción:</strong> ${sede.descripcion}<br>
+                    <strong>Fecha de Creación:</strong> ${sede.fechaCreacion}<br>
+                    <strong>Provincia:</strong> ${sede.provincia}<br>
+                    <strong>Cantón:</strong> ${sede.canton}<br>
+                    <strong>Distrito:</strong> ${sede.distrito}<br>
+                    <strong>Ubicación:</strong> ${sede.ubicacion}<br>
+                </p>
+                <button type="button" class="btn btn-primary mx-2 b-color1 btn-editar">Editar</button>
+            </div>
+        </div>
+    </div>
+`;
+
+        $('.container .row').append(nuevaSedeHTML);
+    }
 
 });
 
-/* var registrarSede = new RegistrarSede();
-registrarSede.InitView(); */
+
+$('.container .row').on('click', '.btn-editar', function () {
+    $('#registroModal').modal('show');
+});
