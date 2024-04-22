ebf6546b47a9e46db6d50b8165faa5ffcfc3afa6
ffumero03 <148408412+ffumero03@users.noreply.github.com>
2024-04-21 20:58:20 -0600

    Registrar Expediente funcional, ademas la vista de mostrar expediente que todavia no es funcional

--- SIMEPCI-Web/Controllers/ExpedientesController.cs
+++ SIMEPCI-Web/Controllers/ExpedientesController.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 
 namespace SIMEPCI_Web.Controllers
 {
@@ -9,26 +7,6 @@ namespace SIMEPCI_Web.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult InfoPacientes()
-        {
-            var pacientes = new List<InformacionPaciente>
-            {
-                new InformacionPaciente { Nombre="Alonso Rojas Araya", FechaNacimiento = new DateTime(1995, 01, 29), Direccion="Urbanización El Roble, Casa #4, Mercedes Norte, Heredia", Diagnostico="Covid 19, virus no especificado - 04/12/2022"}
-            };
-
-            return View(pacientes);
-        }
-
-        public IActionResult NotasMedicasEnfermeria()
-        {
-            var notas = new List<Anotaciones>
-            {
-                new Anotaciones { NotasMedicas="Quisque a ultrices lectus. Nulla ultrices enim tempor tincidunt iaculis. Nunc sed nisi non sem varius congue. Aliquam ut leo eu odio luctus accumsan a consectetur tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus.", NotasEnfermeria="Quisque a ultrices lectus. Nulla ultrices enim tempor tincidunt iaculis. Nunc sed nisi non sem varius congue. Aliquam ut leo eu odio luctus accumsan a consectetur tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus."}
-            };
-
-            return View(notas);
         }
 
         public IActionResult RegistrarExpediente()
@@ -48,20 +26,6 @@ namespace SIMEPCI_Web.Controllers
         }
     }
 
-    public class InformacionPaciente
-    {
-        public string Nombre { get; set; }
-        public DateTime FechaNacimiento { get; set; }
-        public string Direccion { get; set; }
-        public string Diagnostico { get; set; }
-    }
-
-    public class Anotaciones
-    {
-        public string NotasMedicas { get; set; }
-        public string NotasEnfermeria { get; set; }
-    }
-
     public class Expediente
     {
         public int IdPaciente { get; set; }
@@ -70,5 +34,4 @@ namespace SIMEPCI_Web.Controllers
         public string HistorialMedico { get; set; }
     }
 }
-
 
