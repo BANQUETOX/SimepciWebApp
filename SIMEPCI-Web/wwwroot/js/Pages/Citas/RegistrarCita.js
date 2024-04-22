a83ec306f3c52d87c0a7937a0eb6d22a170bad46
gacunal <gacunal@ucenfotec.ac.cr>
2024-04-21 22:38:20 -0600

    cambios RegistrarCita

--- SIMEPCI-Web/wwwroot/js/Pages/Citas/RegistrarCita.js
+++ SIMEPCI-Web/wwwroot/js/Pages/Citas/RegistrarCita.js
@@ -1,49 +1,52 @@
-﻿var IdEspecialidadSeleccionada = "";
+﻿var nombreCompleto = "";
+var IdEspecialidadSeleccionada = "";
 var IdSedeSeleccionada = "";
 var rol = sessionStorage.getItem('rol');
+var calendar;
+
 function RegistrarCita() {
     this.InitView = function () {
         this.habilitarCampos();
-        this.GetAllEspecialidades();
         this.GetAllSedes();
-        this.SeleccionarEspecialidad();
         this.SeleccionarSede();
+        this.SeleccionarEspecialidad();
         $('#btnCuposDisponiblesDoctor').click(() => {
             this.CuposDisponibles();
         });
+        $('#btnCuposDisponiblesPaciente').click(() => {
+            this.CuposDisponiblesPaciente();
+        });
+        $('#btnBuscar').click(() => {
+            this.GetPacienteCorreo();
+        });
         console.log(rol);
-
     }
-
     this.SeleccionarEspecialidad = function () {
         $('#especialidad').change(function () {
-            var especialidadSeleccionadaId = $(this).find(":selected").attr('id');
-            IdEspecialidadSeleccionada = especialidadSeleccionadaId;
+            IdEspecialidadSeleccionada = $(this).find(":selected").attr('id');
             console.log("ID de especialidad seleccionada :", IdEspecialidadSeleccionada);
         });
     }
-
     this.SeleccionarSede = function () {
-        $('#sede').change(function () {
-            var sedeSeleccionadaId = $(this).find(":selected").attr('id');
-            IdSedeSeleccionada = sedeSeleccionadaId;
+        $('#sede').change(() => {
+            IdSedeSeleccionada = $('#sede option:selected').val();
             console.log("ID de sede seleccionada :", IdSedeSeleccionada);
+            this.GetAllEspecialidadesBySede();
         });
     }
-
-    this.GetAllEspecialidades = function () {
-        var url_base = 'https://simepciapii.azurewebsites.net/api/EspecialidadMedica/GetAllEspecialidadesMedicas';
+    this.GetAllEspecialidadesBySede = function () {
+        var url_base = 'https://simepciapii.azurewebsites.net/api/EspecialidadMedica/GetEspecialidadesMedicasSede?idSede=';
         $.ajax({
-            url: url_base,
+            url: url_base + IdSedeSeleccionada,
             method: 'GET',
             contentType: 'application/json;charset=utf-8',
             dataType: "json"
         }).done(function (result) {
             var listaEspecialidades = $('#especialidad');
+            listaEspecialidades.empty();
             var select = $('<select>');
 
             $('<option>').text('Seleccione una especialidad').attr('disabled', 'disabled').attr('selected', 'selected').appendTo(select);
-
             $.each(result, function (index, especialidad) {
                 $('<option>').text(especialidad.nombre).attr('id', especialidad.id).val(especialidad.id).appendTo(select);
             });
@@ -52,7 +55,6 @@ function RegistrarCita() {
             console.log(error);
         });
     }
-
     this.GetAllSedes = function () {
         var url_base = 'https://simepciapii.azurewebsites.net/api/Sede/GetAllSedes';
         $.ajax({
@@ -72,11 +74,7 @@ function RegistrarCita() {
             console.log(error);
         });
     }
-
     this.CuposDisponibles = function () {
-        console.log("ID de especialidad seleccionada (desde otro método):", IdEspecialidadSeleccionada);
-        console.log("ID de sede seleccionada (desde otro método):", IdSedeSeleccionada);
-
         var url_base = 'https://simepciapii.azurewebsites.net/api/Cita/GetCitasReservadas?idEspecialidad=';
         $.ajax({
             url: url_base + IdEspecialidadSeleccionada + '&idSede=' + IdSedeSeleccionada,
@@ -87,17 +85,43 @@ function RegistrarCita() {
             var eventosFiltrados = response.map(function (cita) {
                 return {
                     id: cita.id,
-                    title: 'Cita',
+                    title: 'Cita ' + cita.especialidad,
                     start: cita.horaInicio,
                     end: cita.horaFinal
                 };
             });
-
             var calendarEl = document.getElementById('calendarioDisponible');
-            var calendar = new FullCalendar.Calendar(calendarEl, {
+            calendar = new FullCalendar.Calendar(calendarEl, {
+                headerToolbar: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'Week,dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+                },
+                customButtons: {
+                    Week: {
+                        text: 'Week',
+                        click: function () {
+                            calendar.changeView('timeGridWeek');
+                        }
+                    }
+                },
                 initialView: 'timeGridWeek',
                 slotDuration: '00:30:00',
-                events: eventosFiltrados
+                events: eventosFiltrados,
+                selectable: true,
+                select: function (info) {
+                    var horaInicio = info.startStr;
+                    var horaFinal = moment(info.start).add(30, 'minutes').format();
+                    var correoPaciente = $('#correoUsuario').val();
+                    var cita = {
+                        correoPaciente: correoPaciente,
+                        horaInicio: horaInicio,
+                        horaFinal: horaFinal,
+                        idSede: IdSedeSeleccionada,
+                        idEspecialidad: IdEspecialidadSeleccionada
+                    };
+                    verificarDisponibilidad(cita);
+                }
             });
             calendar.render();
         }).fail(function (error) {
@@ -105,10 +129,7 @@ function RegistrarCita() {
         });
     }
     this.CuposDisponiblesPaciente = function () {
-        console.log("ID de especialidad seleccionada (desde otro método):", IdEspecialidadSeleccionada);
-        console.log("ID de sede seleccionada (desde otro método):", IdSedeSeleccionada);
-
-        var url_base = 'https://simepciapii.azurewebsites.net/api/Cita/GetCitasPaciente?correoPaciente=';
+        var url_base = 'https://simepciapii.azurewebsites.net/api/Cita/GetCitasReservadas?idEspecialidad=';
         $.ajax({
             url: url_base + IdEspecialidadSeleccionada + '&idSede=' + IdSedeSeleccionada,
             method: 'GET',
@@ -118,29 +139,96 @@ function RegistrarCita() {
             var eventosFiltrados = response.map(function (cita) {
                 return {
                     id: cita.id,
-                    title: 'Cita',
+                    title: 'Cita ' + cita.especialidad,
                     start: cita.horaInicio,
                     end: cita.horaFinal
                 };
             });
-
             var calendarEl = document.getElementById('calendarioDisponible');
-            var calendar = new FullCalendar.Calendar(calendarEl, {
+            calendar = new FullCalendar.Calendar(calendarEl, {
+                headerToolbar: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'Week,dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+                },
+                customButtons: {
+                    Week: {
+                        text: 'Week',
+                        click: function () {
+                            calendar.changeView('timeGridWeek');
+                        }
+                    }
+                },
                 initialView: 'timeGridWeek',
                 slotDuration: '00:30:00',
-                events: eventosFiltrados
+                events: eventosFiltrados,
+                selectable: true,
+                select: function (info) {
+                    var horaInicio = info.startStr;
+                    var horaFinal = moment(info.start).add(30, 'minutes').format();
+                    var correoPaciente = sessionStorage.getItem('correo');
+                    var cita = {
+                        correoPaciente: correoPaciente,
+                        horaInicio: horaInicio,
+                        horaFinal: horaFinal,
+                        idSede: IdSedeSeleccionada,
+                        idEspecialidad: IdEspecialidadSeleccionada
+                    };
+                    verificarDisponibilidad(cita);
+                }
             });
             calendar.render();
         }).fail(function (error) {
             console.error('Error al obtener citas reservadas:', error);
         });
     }
+    function verificarDisponibilidad(cita) {
+        //Pendiente editar mensajes para mostrar la info antes de confirmar o cancelar
+        var horaInicioFormateada = moment(cita.horaInicio).format('YYYY-MM-DD HH:mm');
+        Swal.fire({
+            title: '¿Estás seguro de registrar la cita?',
+            text: 'Una vez registrada, no se podrá deshacer esta acción' + horaInicioFormateada,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Sí, registrar',
+            cancelButtonText: 'No, cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                crearCita(cita);
+            } else {
+                Swal.fire({
+                    title: 'Acción cancelada',
+                    icon: 'info',
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            }
+        });
+    }
+    this.GetPacienteCorreo = function () {
+        var correo = $('#correoUsuario').val();
+        var url_base = 'https://simepciapii.azurewebsites.net/api/Usuario/GetUsuarioByCorreo?correo=';
+        $.ajax({
+            url: url_base + correo,
+            method: 'GET',
+            contentType: 'application/json;charset=utf-8',
+            dataType: 'json'
+        }).done(function (result) {
+            nombreCompleto = result.nombre + ' ' + result.primerApellido + ' ' + result.segundoApellido;
+            console.log(nombreCompleto);
+            $('#nombreUsuario').val(nombreCompleto);
+        }).fail(function (error) {
+            console.log(error);
+        });
+    }
     this.habilitarCampos = function () {
         switch (rol) {
             case 'Paciente':
-                campoCorreo.style.display = 'none';
-                textoCorreo.style.display = 'none';
                 btnCuposDisponiblesDoctor.style.display = 'none';
+                textoCorreo.style.display = 'none';
+                campoCorreo.style.display = 'none';
+                campoNombre.style.display = 'none';
+                btnBuscar.style.display = 'none';
                 break;
             case 'Enfermero':
 
@@ -159,7 +247,32 @@ function RegistrarCita() {
         }
     }
 }
-
+function crearCita(cita) {
+    var url_base = 'https://simepciapii.azurewebsites.net/api/Cita/CrearCita';
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        url: url_base,
+        contentType: 'application/json;charset=utf-8',
+        data: JSON.stringify(cita),
+        hasContent: true
+    }).done(function (result) {
+        console.log(result)
+        Swal.fire({
+            icon: 'success',
+            title: 'Cita creada correctamente',
+            showConfirmButton: false,
+            timer: 2000
+        }).then(function () {
+            window.location = "/Citas/CitasProgramadas";
+        });
+    }).fail(function (error) {
+        console.log(error)
+    });
+}
 $(document).ready(function () {
     var view = new RegistrarCita();
     view.InitView();
