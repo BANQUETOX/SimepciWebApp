3f12ab28c4b2652ce4dfc76d26df947fab09e655
quened29 <kriverac@ucenfotec.ac.cr>
2024-04-22 00:01:27 -0600

    creacion de tabla de ganancias mensuales con su respectivo js

--- SIMEPCI-Web/wwwroot/js/Pages/ReportesFinancieros/ReportesFinancieros.js
+++ SIMEPCI-Web/wwwroot/js/Pages/ReportesFinancieros/ReportesFinancieros.js
@@ -19,3 +19,42 @@
     });
 });
 
+// Reportes mensuales 
+$(document).ready(function () {
+    .ajax({
+        url: 'https://simepciapii.azurewebsites.net/api/Factura/GetReportesFinancieros',
+        method: 'GET',
+        success: function (data) {
+            poblarTabla(data);
+        },
+        error: function (xhr, status, error) {
+            console.error('Error al obtener datos del API:', error);
+        }
+    });
+
+    function poblarTabla(data) {
+        var tbody = $('#tablaGanancias tbody');
+        tbody.empty();
+
+        data.forEach(function (gananciasMes) {
+            var fila = $('<tr>');
+
+            fila.append($('<td>').text(formatDate(gananciasMes.mes)));
+            fila.append($('<td>').text(gananciasMes.gananciaCitas));
+            fila.append($('<td>').text(gananciasMes.gananciaServicios));
+            fila.append($('<td>').text(gananciasMes.gananciasTotales));
+
+            tbody.append(fila);
+        });
+    }
+
+    function formatDate(dateString) {
+        var date = new Date(dateString);
+        var options = { month: 'numeric', year: 'numeric' };
+        return date.toLocaleDateString('es-ES', options);
+    }
+});
+
+function truncateText(text, maxLength) {
+    return text.length > maxLength ? text.substring(0, maxLength) + '...' : text;
+}
